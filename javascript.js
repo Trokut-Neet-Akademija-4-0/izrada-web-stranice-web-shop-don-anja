649b7ecb9dbab047e2770d6b4356242a9fc8f416
Anja <145154991+don-anja@users.noreply.github.com>
2024-01-08 15:46:32 +0100

    add iframe

---
+++ javascript.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  function adjustIframeHeight(iframe) {
+    iframe.onload = function () {
+      var frameContent =
+        iframe.contentDocument || iframe.contentWindow.document;
+      var object = frameContent.querySelector("object");
+      var frameBody = frameContent.querySelector("body");
+      var height = object.offsetHeight;
+      frameBody.style.height = height + "px";
+    };
+  }
+  var iframes = document.querySelectorAll(".auto-height-iframe");
+  iframes.forEach(adjustIframeHeight);
+});
