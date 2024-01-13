5a7b57281af5812c36a42e2d48208dcfaac78d18
Anja <145154991+don-anja@users.noreply.github.com>
2024-01-13 13:42:42 +0100

    update

--- javascript.js
+++ javascript.js
@@ -5,11 +5,13 @@ document.addEventListener("DOMContentLoaded", function () {
         iframe.contentDocument || iframe.contentWindow.document;
       var object = frameContent.querySelector("object");
       var frameBody = frameContent.querySelector("body");
+      var body = frameContent.querySelector("body");
       var height = object.offsetHeight;
       frameBody.style.height = height + "px";
+      body.style.margin = 0 + "px";
+      iframe.style.height = height + 5 + "px";
     };
   }
   var iframes = document.querySelectorAll(".auto-height-iframe");
-  console.log(iframes);
   iframes.forEach(adjustIframeHeight);
 });
