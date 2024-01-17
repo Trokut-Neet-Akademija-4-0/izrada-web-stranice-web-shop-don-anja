1219f82922428e05279f9c374df5d5355dda5322
Anja <145154991+don-anja@users.noreply.github.com>
2024-01-17 12:38:19 +0100

    create about page

--- javascript.js
+++ javascript.js
@@ -5,11 +5,9 @@ document.addEventListener("DOMContentLoaded", function () {
         iframe.contentDocument || iframe.contentWindow.document;
       var object = frameContent.querySelector("object");
       var frameBody = frameContent.querySelector("body");
-      var body = frameContent.querySelector("body");
       var height = object.offsetHeight;
-      frameBody.style.height = height + "px";
-      body.style.margin = 0 + "px";
-      iframe.style.height = height + 5 + "px";
+      frameBody.style.margin = 50;
+      iframe.style.height = height + "px";
     };
   }
   var iframes = document.querySelectorAll(".auto-height-iframe");
